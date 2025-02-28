d2380372b7c303b6da2a905e432d11e726168c09
Tommy Burkett <158101222+tommy-burkett@users.noreply.github.com>
2025-02-27 23:56:00 -0500

    Update datagen.py

--- src/datagen.py
+++ src/datagen.py
@@ -1,23 +1,30 @@
 import numpy as np
+import os
 from src.helpers import PATH_DATA
 
+# half_deck_size to simulate black and red cards
 HALF_DECK_SIZE = 26
 
-def get_decks(n_decks: int,
-              seed: int, 
-              half_deck_size: int = HALF_DECK_SIZE 
-              ) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Efficiently generate `n_decks` shuffled decks using NumPy.
+
+def get_decks(n_decks: int, 
+              seed: int,
+              half_deck_size: int = HALF_DECK_SIZE
+             ) -> tuple[np.ndarray, np.ndarray]:
     
-    Returns:
-        decks (np.ndarray): 2D array of shape (n_decks, num_cards), 
-        each row is a shuffled deck.
     """
-    init_deck = [0]*half_deck_size + [1]*half_deck_size
+        Efficiently generate `n_decks` shuffled decks using NumPy.
+    
+        Returns:
+            decks (np.ndarray): 2D array of shape (n_decks, num_cards), 
+            each row is a shuffled deck.
+
+    """
+    
+    init_deck = [0]*half_deck_size + [1]*half_deck_size  # Base deck
     decks = np.tile(init_deck, (n_decks, 1))
     rng = np.random.default_rng(seed)
     rng.permuted(decks, axis=1, out=decks)
+    
     return decks
 
 
@@ -59,3 +66,4 @@ def store_data(n_decks: int,
     # Save the filename and decks if filename does not exist yet
     else:
         np.save(filename, decks)
+
