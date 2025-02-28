240b270593a9207c717725e40ae3a2ae3f230efd
Tommy Burkett <158101222+tommy-burkett@users.noreply.github.com>
2025-02-27 23:56:19 -0500

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,19 +1,26 @@
 from typing import Callable
 from datetime import datetime as dt
 
+DEBUG = True
+
 PATH_DATA = 'data'
 PATH_FIGURES = 'figures'
 
-def debugger_factory(show_args = True) -> Callable:
-    def debugger(func: Callable) -> Callable:
-        def wrapper(*args, **kwargs):
-            if show_args:
-                print(f'{func.__name__} was called with:')
-                print('Positional arguments:\n', args)
-                print('Keyword arguments:\n', kwargs)
-            t0 = dt.now()
-            results = func(*args, **kwargs)
-            print(f'{func.__name__} ran for {dt.now() - t0}')
-            return results
+
+def debugger_factory(show_args=True) -> Callable:
+    def debugger(func:Callable) -> Callable:
+        if DEBUG:
+            def wrapper(*args, **kwargs):
+                if show_args:
+                    print(f'{func.__name__} was called with:')
+                    print('Positional arguments:\n', args)
+                    print('Keyword arguments:\n', kwargs)
+        
+                t0 = dt.now()
+                results = func(*args, **kwargs)
+                print(f'{func.__name__} ran for {dt.now()-t0}')
+                return results
+        else:
+            return func
         return wrapper
     return debugger
